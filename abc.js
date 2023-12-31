2e0dd5b9ed6c40c9e74bc94c5f479108097a1e8d
deb <43925933+lil-bunny@users.noreply.github.com>
2023-12-31 12:57:19 +0530

    Create abc.js

---
+++ abc.js
@@ -0,0 +1,70 @@
+import configureMockStore from 'redux-mock-store';
+import thunk from 'redux-thunk';
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { onAuth } from './yourAuthModule'; // Replace with the actual path
+
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+const mock = new MockAdapter(axios);
+
+describe('onAuth action creator', () => {
+  afterEach(() => {
+    mock.reset();
+  });
+
+  it('dispatches authSuccess action on successful authentication', () => {
+    const email = 'test@example.com';
+    const password = 'password123';
+    const isSignup = true;
+    const store = mockStore({});
+
+    const authData = {
+      email,
+      password,
+      returnSecureToken: true
+    };
+
+    const responseData = {
+      idToken: 'mockedToken',
+      localId: 'mockedUserId',
+      expiresIn: 3600,
+    };
+
+    mock.onPost(/.*/).reply(200, responseData);
+
+    const expectedActions = [
+      { type: 'AUTH_START' },
+      { type: 'AUTH_SUCCESS', idToken: 'mockedToken', userId: 'mockedUserId' },
+      // Add your expected actions for checkAuthTimeout if needed
+    ];
+
+    return store.dispatch(onAuth(email, password, isSignup)).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+
+  it('dispatches authFail action on failed authentication', () => {
+    const email = 'test@example.com';
+    const password = 'password123';
+    const isSignup = true;
+    const store = mockStore({});
+
+    const authData = {
+      email,
+      password,
+      returnSecureToken: true
+    };
+
+    mock.onPost(/.*/).reply(400, { error: 'Mocked error' });
+
+    const expectedActions = [
+      { type: 'AUTH_START' },
+      { type: 'AUTH_FAIL', error: 'Mocked error' },
+    ];
+
+    return store.dispatch(onAuth(email, password, isSignup)).then(() => {
+      expect(store.getActions()).toEqual(expectedActions);
+    });
+  });
+});
